7fa890b50f36c3b6c75d538821da64f242b91de6
Abuddabi <web.abudabi@gmail.com>
2023-06-24 15:16:23 -0500

    practice with form handling. 2 way binding for variables

---
+++ 3.39_components-practice/src/components/NewExpense/ExpenseForm.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+    const emptyFormData = {
+        title: "",
+        amount: "",
+        date: "",
+    };
+    const [formData, setFormData] = useState(emptyFormData);
+
+    const formChangeHandler = (e, valueName) => {
+        setFormData((prevState) => {
+            let newFormData = { ...prevState };
+            newFormData[valueName] = e.target.value;
+
+            return newFormData;
+        });
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        formData.date = new Date(formData.date);
+        console.log(formData);
+
+        setFormData(emptyFormData);
+    };
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label htmlFor="">Title</label>
+                    <input
+                        type="text"
+                        onChange={(e) => formChangeHandler(e, "title")}
+                        value={formData.title}
+                    />
+                </div>
+                <div className="new-expense__control">
+                    <label htmlFor="">Amount</label>
+                    <input
+                        type="number"
+                        onChange={(e) => formChangeHandler(e, "amount")}
+                        value={formData.amount}
+                        min="0.01"
+                        step="0.01"
+                    />
+                </div>
+                <div className="new-expense__control">
+                    <label htmlFor="">Date</label>
+                    <input
+                        type="date"
+                        onChange={(e) => formChangeHandler(e, "date")}
+                        value={formData.date}
+                        min="2019-01-01"
+                        max="2022-12-31"
+                    />
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+    );
+};
+
+export default ExpenseForm;
