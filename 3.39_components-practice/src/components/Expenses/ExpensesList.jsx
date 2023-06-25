a9628d56b5e68ef978f9861b9b08eadd927862e2
Abuddabi <web.abudabi@gmail.com>
2023-06-25 10:23:39 -0500

    Another way of handle if coditions inside a component

--- 3.39_components-practice/src/components/Expenses/ExpensesList.jsx
+++ 3.39_components-practice/src/components/Expenses/ExpensesList.jsx
@@ -2,15 +2,17 @@ import "./ExpensesList.css";
 import ExpenseItem from "./ExpenseItem";
 
 const ExpensesList = ({ filteredExpenses }) => {
-    let expensesContent = <p>No expenses found</p>;
-
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem expense={expense} key={expense.id} />
-        ));
+    if (filteredExpenses.length === 0) {
+        return <p>No expenses found</p>;
     }
 
-    return <ul className="expenses-list">{expensesContent}</ul>;
+    return (
+        <ul className="expenses-list">
+            {filteredExpenses.map((expense) => (
+                <ExpenseItem expense={expense} key={expense.id} />
+            ))}
+        </ul>
+    );
 };
 
 export default ExpensesList;
