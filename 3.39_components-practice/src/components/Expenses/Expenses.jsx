92228adba6586b0b938e6e8877f30c0f7598ce14
Abuddabi <web.abudabi@gmail.com>
2023-06-24 22:37:24 -0500

    assignment 2

--- 3.39_components-practice/src/components/Expenses/Expenses.jsx
+++ 3.39_components-practice/src/components/Expenses/Expenses.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = ({ expenses }) => {
+    const years = [2022, 2021, 2020, 2019];
+    const [filteredYear, setFilteredYear] = useState(years[2]);
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+        console.log(selectedYear);
+    };
+
     return (
         <Card className="expenses">
+            <ExpensesFilter
+                selected={filteredYear}
+                years={years}
+                onChangeFilter={filterChangeHandler}
+            />
             {expenses.map((expense, index) => (
                 <ExpenseItem expense={expense} key={index} />
             ))}
---
+++ 3.39_components-practice/src/components/Expenses/ExpensesFilter.jsx
@@ -0,0 +1,24 @@
+import "./ExpensesFilter.css";
+
+const ExpensesFilter = (props) => {
+    const changeHandler = (e) => {
+        props.onChangeFilter(e.target.value);
+    };
+
+    return (
+        <div className="expenses-filter">
+            <div className="expenses-filter__control">
+                <label>Filter by year</label>
+                <select value={props.selected} onChange={changeHandler}>
+                    {props.years.map((year, id) => (
+                        <option key={id} value={year}>
+                            {year}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default ExpensesFilter;
