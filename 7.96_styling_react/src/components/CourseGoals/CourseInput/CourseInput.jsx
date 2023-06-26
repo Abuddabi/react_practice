481a86d269321281261e589fadd3315f40cb5170
Abuddabi <web.abudabi@gmail.com>
2023-06-25 23:44:27 -0500

    starting project Section 8

--- 7.96_styling_react/src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ 7.96_styling_react/src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -8,13 +8,15 @@ const CourseInput = (props) => {
     const [isValid, setIsValid] = useState(true);
 
     const goalInputChangeHandler = (event) => {
-        if (event.target.value.trim().length > 0) setIsValid(true);
+        if (event.target.value.trim().length > 0) {
+            setIsValid(true);
+        }
         setEnteredValue(event.target.value);
     };
 
     const formSubmitHandler = (event) => {
         event.preventDefault();
-        if (!enteredValue.trim().length) {
+        if (enteredValue.trim().length === 0) {
             setIsValid(false);
             return;
         }
