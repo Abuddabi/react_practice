69c091d2689ac005945b1565f806ba8c56b8e143
Abuddabi <web.abudabi@gmail.com>
2023-07-07 10:40:20 -0500

    Modal

--- food_order_app/src/App.jsx
+++ food_order_app/src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 const App = () => {
+    const [activeCart, setActiveCart] = useState(false);
+
+    const showCartHandler = () => {
+        setActiveCart(true);
+    };
+
+    const hideCartHandler = () => {
+        setActiveCart(false);
+    };
+
     return (
         <>
-            <Header />
+            {activeCart && <Cart onClose={hideCartHandler} />}
+            <Header onOpenCartClick={showCartHandler} />
             <main>
                 <Meals />
             </main>
