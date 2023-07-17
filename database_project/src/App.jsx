5fc9655e9983ec08bbc5e376df47e8a1e81059a3
Abuddabi <web.abudabi@gmail.com>
2023-07-17 17:17:26 -0500

    Tasks instead of films

--- database_project/src/App.jsx
+++ database_project/src/App.jsx
@@ -1,83 +1,52 @@
 import { useState, useEffect, useCallback } from "react";
-import MoviesList from "./components/MoviesList";
 import "./App.css";
-import AddMovie from "./components/AddMovie";
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
 
 const swapiURL = "https://swapi.dev/api/";
 const firebaseDB_URL =
     "https://react-practice-1-6bd4d-default-rtdb.firebaseio.com/";
 
 function App() {
-    const [filmList, setFilmList] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [tasks, setTasks] = useState([]);
 
-    const fetchMoviesHandler = useCallback(async () => {
+    const fetchTasks = async (taskText) => {
         setIsLoading(true);
         setError(null);
         try {
-            const response = await fetch(firebaseDB_URL + "movies.json");
-            if (!response.ok) throw new Error("Something went wrong!");
+            const response = await fetch(firebaseDB_URL + "tasks.json");
+            if (!response.ok) throw new Error("Request failed!");
             const data = await response.json();
-            console.log(data);
-            const loadedMovies = [];
-            for (const key in data) {
-                loadedMovies.push({
-                    id: key,
-                    title: data[key].title,
-                    openingText: data[key].openingText,
-                    releaseDate: data[key].releaseDate,
-                });
+            const loadedTasks = [];
+            for (const taskKey in data) {
+                loadedTasks.push({ id: taskKey, text: data[taskKey].text });
             }
-            setFilmList(loadedMovies);
-        } catch (error) {
-            setError(error.message);
+            setTasks(loadedTasks);
+        } catch (err) {
+            setError(err.message || "Something went wrong!");
         }
         setIsLoading(false);
-    }, []);
-
-    useEffect(() => {
-        fetchMoviesHandler();
-    }, [fetchMoviesHandler]);
-
-    const addMovieHandler = async (movie) => {
-        try {
-            const response = await fetch(firebaseDB_URL + "movies.json", {
-                method: "POST",
-                body: JSON.stringify(movie),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-
-            const data = await response.json();
-            console.log(data);
-            fetchMoviesHandler();
-        } catch (err) {
-            console.error(err);
-        }
     };
 
-    const moviesContent = isLoading ? (
-        <p>Movies are loading...</p>
-    ) : error ? (
-        <p style={{ color: "red" }}>{error}</p>
-    ) : filmList.length > 0 ? (
-        <MoviesList movies={filmList} />
-    ) : (
-        // if !isLoading && !error && !filmList > 0
-        <p>Found no movies.</p>
-    );
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    const taskAddHandler = (task) => {
+        setTasks((prevTasks) => prevTasks.concat(task));
+    };
 
     return (
         <>
-            <section>
-                <AddMovie onAddMovie={addMovieHandler} />
-            </section>
-            <section>
-                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-            </section>
-            <section>{moviesContent}</section>
+            <NewTask onAddTask={taskAddHandler} />
+            <Tasks
+                items={tasks}
+                loading={isLoading}
+                error={error}
+                onFetch={fetchTasks}
+            />
         </>
     );
 }
