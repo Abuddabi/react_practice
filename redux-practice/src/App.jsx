dbc05817212f5162890f096575ba5f14cef30c16
Abuddabi <web.abudabi@gmail.com>
2023-07-23 16:32:50 -0500

    finished section

--- redux-practice/src/App.jsx
+++ redux-practice/src/App.jsx
@@ -1,7 +1,28 @@
+import { useSelector } from "react-redux";
+
 import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-    return <Counter />;
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+    const content = isAuthenticated ? (
+        <>
+            <UserProfile />
+            <Counter />
+        </>
+    ) : (
+        <Auth />
+    );
+
+    return (
+        <>
+            <Header />
+            {content}
+        </>
+    );
 }
 
 export default App;
