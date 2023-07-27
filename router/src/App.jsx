21ba223d14381bb47e020a7513158935c7a9d417
Abuddabi <web.abudabi@gmail.com>
2023-07-27 17:59:08 -0500

    added navigation

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,13 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <HomePage />,
+        element: <RootLayout />,
+        children: [
+            { path: "/", element: <HomePage /> },
+            { path: "/products", element: <ProductsPage /> },
+        ],
     },
-    { path: "/products", element: <ProductsPage /> },
 ]);
 
 const App = () => {
