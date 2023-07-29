2ad753833f7b737c9a79621d5cb29b9ed49d6dde
Abuddabi <web.abudabi@gmail.com>
2023-07-29 13:28:51 -0500

    added delete event

--- router2/frontend/src/pages/EventDetail.js
+++ router2/frontend/src/pages/EventDetail.js
@@ -1,21 +1,45 @@
-import { json, useRouteLoaderData } from "react-router-dom";
-import EventItem from "../components/EventItem";
+import { useRouteLoaderData, json, redirect } from 'react-router-dom';
 
-const EventDetailPage = () => {
+import EventItem from '../components/EventItem';
+
+function EventDetailPage() {
     const data = useRouteLoaderData('event-detail');
 
-    return <>
-        <EventItem event={data.event} />
-    </>;
+    return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
 
 export async function loader({ request, params }) {
     const id = params.id;
+
     const response = await fetch('http://localhost:8080/events/' + id);
 
     if (!response.ok) {
-        throw json({ message: "Could not fetch details for selected event." }, { status: 500 });
-    } else return response;
+        throw json(
+            { message: 'Could not fetch details for selected event.' },
+            {
+                status: 500,
+            }
+        );
+    } else {
+        return response;
+    }
+}
+
+export async function action({ params, request }) {
+    const id = params.id;
+    const response = await fetch('http://localhost:8080/events/' + id, {
+        method: request.method,
+    });
+
+    if (!response.ok) {
+        throw json(
+            { message: 'Could not delete event.' },
+            {
+                status: 500,
+            }
+        );
+    }
+    return redirect('/events');
 }
