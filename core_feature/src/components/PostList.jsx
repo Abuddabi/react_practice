eb0aabc82da98db40bad6dc3f49de01ef1910fe3
Abuddabi <web.abudabi@gmail.com>
2023-08-15 18:24:56 -0500

    working with backend

--- core_feature/src/components/PostList.jsx
+++ core_feature/src/components/PostList.jsx
@@ -2,46 +2,78 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import Modal from "./Modal";
 import classes from "./PostList.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const backendUrl = "http://localhost:8080/posts";
 
 const PostList = (props) => {
-    const [posts, setPosts] = useState([
-        // {
-        //     body: "React.js is good!",
-        //     author: "Yegor",
-        // },
-        // {
-        //     body: "Test text",
-        //     author: "Test",
-        // },
-    ]);
+    const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            setIsFetching(true);
+            try {
+                const response = await fetch(backendUrl);
+                const resData = await response.json();
+                setPosts(resData.posts);
+            } catch (error) {
+                console.error("Error fetching data: ", error);
+            }
+            setIsFetching(false);
+        })();
+    }, []);
 
     const addPostHandler = (post) => {
-        setPosts((prev) => [...prev, post]);
+        fetch(backendUrl, {
+            method: "POST",
+            body: JSON.stringify(post),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        setPosts((prev) => [post, ...prev]);
     };
 
     const deletePostHandler = (id) => {
-        setPosts((prev) => prev.filter((post) => post.body !== id));
+        fetch(backendUrl, {
+            method: "DELETE",
+            body: JSON.stringify({ id: id }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        setPosts((prev) => prev.filter((post) => post.id !== id));
     };
 
-    const postsContent =
-        posts.length > 0 ? (
+    let postsContent = "";
+    if (isFetching) {
+        postsContent = (
+            <div style={{ textAlign: "center", color: "white" }}>
+                <p>Loading posts...</p>
+            </div>
+        )   ;
+    } else if (posts.length > 0) {
+        postsContent = (
             <ul className={classes.posts}>
                 {posts.map((post) => (
                     <Post
-                        onClick={deletePostHandler.bind(null, post.body)}
+                        onClick={deletePostHandler.bind(null, post.id)}
                         key={post.body}
                         name={post.author}
                         text={post.body}
                     />
                 ))}
             </ul>
-        ) : (
+        );
+    } else if (posts.length === 0) {
+        postsContent = (
             <div style={{ textAlign: "center", color: "white" }}>
                 <h2>There are no posts yet.</h2>
                 <p>Start adding some!</p>
             </div>
         );
+    }
 
     return (
         <>
--- dummy-backend/app.js
+++ dummy-backend/app.js
@@ -10,15 +10,24 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   // Attach CORS headers
   // Required when using a detached backend (that runs on a different domain)
+
+  // Set allowed origins
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  // Set allowed headers
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  
+  // Set allowed HTTP methods
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  
+  // Allow credentials (if needed)
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
 app.get('/posts', async (req, res) => {
   const storedPosts = await getStoredPosts();
-  // await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
+  await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
   res.json({ posts: storedPosts });
 });
 
@@ -38,6 +47,14 @@ app.post('/posts', async (req, res) => {
   const updatedPosts = [newPost, ...existingPosts];
   await storePosts(updatedPosts);
   res.status(201).json({ message: 'Stored new post.', post: newPost });
+});
+
+app.delete('/posts', async (req, res) => {
+  const existingPosts = await getStoredPosts();
+  const id = req.body.id;
+  const updatedPosts = existingPosts.filter(post => post.id !== id);
+  await storePosts(updatedPosts);
+  res.status(201).json({ message: 'Deleted post.', id: id });
 });
 
 app.listen(8080);
